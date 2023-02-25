ee5fc02367b9f9b5a6cdbe12d9337ed3ff45e941
Goomcz <Goomcz@users.noreply.github.com>
2023-02-25 21:24:06 +0900

    Saterday

--- codingTest_MW_Ver2/55.문자열정렬_오름차순/codingtest55.js
+++ codingTest_MW_Ver2/55.문자열정렬_오름차순/codingtest55.js
@@ -3,7 +3,7 @@
 // 단, 같다면 사전 순으로 정렬하라
 // 모든 알파벳은 소문자이다.
 
-let String  = ['abce', 'dbad', 'cdx'];
+let String  = ['abae', 'dbbd', 'cdcs'];
 let n = 2;
 
 let newString = String.sort();
@@ -15,7 +15,21 @@ String.map((val,index) =>{
 });
 console.log(stringMap);
 // 2. Map 객체 만들기까지 ok
-let newstringMap = new Map([...stringMap.entries()].sort((a,b)=> a[1]-b[1]));
+// let newstringMap = new Map([...stringMap.entries()].sort((a,b)=> a[1]-b[1]));
+
+let newstringMap = new Map([...stringMap].sort((a,b) => 
+{if(a[1].charCodeAt()-b[1].charCodeAt() < 0){
+    return -1;
+} else 1;
+})); 
+// 전개 연산자 필수 
+// 이 알고리즘이 이해가 안됨
+
 console.log(newstringMap);
-// let newstringMap = new Map([...stringMap].sort((a,b) => a[1]-b[1])); // 전개 연산자 필수 
-//console.log(newstringMap);
+console.log(newstringMap['0']);
+
+/*String.map((value,index) =>{
+    if(index === newstringMap){
+        
+    }  
+})*/
