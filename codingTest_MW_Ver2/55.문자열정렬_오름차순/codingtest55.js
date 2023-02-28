1f967d8771626d2a41f434d091a5af0ccfbf4eff
Goomcz <Goomcz@users.noreply.github.com>
2023-02-28 21:46:48 +0900

    String treatment

--- codingTest_MW_Ver2/55.문자열정렬_오름차순/codingtest55.js
+++ codingTest_MW_Ver2/55.문자열정렬_오름차순/codingtest55.js
@@ -6,9 +6,8 @@
 let String  = ['abae', 'dbbd', 'cdcs'];
 let n = 2;
 
-let newString = String.sort();
+let newString = String.sort(); // 1. 일단 사전 순으로 정렬
 console.log(newString);
-// 1. 여기까지 ok
 let stringMap = new Map();
 String.map((val,index) =>{
     stringMap.set(index,val[n])
@@ -16,20 +15,17 @@ String.map((val,index) =>{
 console.log(stringMap);
 // 2. Map 객체 만들기까지 ok
 // let newstringMap = new Map([...stringMap.entries()].sort((a,b)=> a[1]-b[1]));
+// 오개념! : sort()는 문자열 정렬/ a-b ~ 형태는 숫자 정렬임
 
 let newstringMap = new Map([...stringMap].sort((a,b) => 
 {if(a[1].charCodeAt()-b[1].charCodeAt() < 0){
     return -1;
 } else 1;
 })); 
+// 3. Map 객체 sort by value 완료
 // 전개 연산자 필수 
 // 이 알고리즘이 이해가 안됨
 
 console.log(newstringMap);
-console.log(newstringMap.keys(0));
+console.log(newstringMap.keys());
 
-/*String.map((value,index) =>{
-    if(index === newstringMap){
-        
-    }  
-})*/
